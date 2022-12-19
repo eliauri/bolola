f4e1ef3016439ff0897f77cd27c1fa034c1cea45
eliauri <georgy.eliauri@yandex.ru>
2022-12-19 16:23:24 +0300

    some fix

--- components/news/NewsPost.jsx
+++ components/news/NewsPost.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import s from '../../styles/news.module.scss'
+import s from '../../styles/home.module.scss'
 
 const NewsPost = ({ post }) => {
     return (
