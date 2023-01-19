f85532ed69b1a9426373a4c44c620382794f6b94
eliauri <georgy.eliauri@yandex.ru>
2023-01-19 17:34:52 +0300

    Изменение регистра

---
+++ components/header/Header.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import cl from 'classnames'
+import s from './header.module.scss';
+
+import Container from '../Container';
+import Navigation from './Navigation';
+import User from './User';
+
+import logo from '../../public/logo.svg'
+import add from '../../public/add-bolik.svg'
+
+
+
+
+
+const Header = () => {
+  const [gamburgerActive, setGamburgerActive] = useState(false);
+
+  useEffect(() => {
+    gamburgerActive ?
+      document.querySelector("body").classList.add("noscroll") :
+      document.querySelector("body").classList.remove("noscroll");
+  }, [gamburgerActive])
+
+  const gamburgerToggle = () => {
+    setGamburgerActive(!gamburgerActive)
+  }
+  return (
+    <header>
+      <Container className='container--noIndex'>
+        <div className={s.header}>
+          <Link href="/">
+            <Image
+              src={logo}
+              alt='logo'
+              width={140}
+              height={30} />
+          </Link>
+          <Navigation />
+          <div className={s.actions}>
+            <User />
+            <Link href="/scaner" className={s.header__scanner}>
+              <Image
+                src={add}
+                alt='add bolik'
+              />
+            </Link>
+            <button
+              className={cl(s.burger, { [s.burgerActive]: gamburgerActive })}
+              onClick={gamburgerToggle}
+            >
+              <span className={s.burger__line}></span>
+            </button>
+            <div className={cl(s.menu, { [s.menuActive]: gamburgerActive })}>
+              <div className={s.menu__content}>
+                <Navigation gamburgerActive setGamburgerActive={setGamburgerActive} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/header/Navigation.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import s from './header.module.scss';
+import Link, { LinkProps } from "next/link"
+import { useRouter } from "next/router"
+import { ActiveLink } from './ActiveLink';
+
+const Navigation = ({ gamburgerActive, setGamburgerActive }) => {
+
+    const links = [
+        {
+            id: 1,
+            title: 'Главная',
+            href: '/',
+        },
+        {
+            id: 2,
+            title: 'Новости',
+            href: '/news',
+        },
+        {
+            id: 3,
+            title: 'Коллекции',
+            href: '/account/collection',
+        },
+        {
+            id: 4,
+            title: 'Продукции',
+            href: '/prod',
+        },
+        {
+            id: 5,
+            title: 'О нас',
+            href: '/about',
+        },
+    ]
+
+    const closeGamburger = () => {
+        if (gamburgerActive) setGamburgerActive(!gamburgerActive);
+    }
+    const { asPath } = useRouter();
+    return (
+        <nav className={s.navigation}>
+            <ul>
+                {links.map((link) => (
+                    <li key={link.id} onClick={closeGamburger}>
+                        <ActiveLink href={link.href} activeClassName={s.active}>
+                            {link.title}
+                        </ActiveLink>
+                        {/* <Link>
+                            {link.title}
+                        </Link> */}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
