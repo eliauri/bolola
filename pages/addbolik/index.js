1c83a4f1f47ef32e079d1793d9327923b6947282
eliauri <georgy.eliauri@yandex.ru>
2023-02-03 19:21:35 +0300

    ошибка версаль 2

--- pages/addbolik/index.js
+++ pages/addbolik/index.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Html5Qrcode } from "html5-qrcode"
 import Container from '../../components/Container'
-import AddBolik from '../../components/add_bolik/addBolik'
+
 
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router';
 import HeaderLayout from '../../components/layout/HeaderLayout'
+import AddBolik from '../../components/add_bolik/addBolik';
 
 
 export default function Skaner() {
